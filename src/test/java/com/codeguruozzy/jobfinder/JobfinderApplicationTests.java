8bca0bc389a87a5cb9e597061eba70588d5a56d8
oscarwergun <127099022+oscarwergun@users.noreply.github.com>
2023-12-05 22:50:05 +0100

    initial setup

---
+++ src/test/java/com/codeguruozzy/jobfinder/JobfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeguruozzy.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
